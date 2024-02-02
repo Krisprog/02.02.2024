e6c430a7545998079ef502b01a6a53b7b52d2d7b
cherkezyan <cherkezyan@itstep.academy>
2024-02-02 11:43:20 +0200

    факториал введенного числа +!

--- 02.02.2024/main.cpp
+++ 02.02.2024/main.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main()
 {
+	// 1 найти факториал введенного числа!
+	int a;
+	cin >> a;
+	int fact = 1;
+	while (a > 0)
+	{
+		fact *= a;
+		a--;
+	}
+	cout << fact << endl;
 
 }
