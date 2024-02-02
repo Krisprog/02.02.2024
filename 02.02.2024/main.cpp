ce1d9b26121f147546b86cae5786c6a49c04fd5f
cherkezyan <cherkezyan@itstep.academy>
2024-02-02 11:48:51 +0200

    Hello world

--- 02.02.2024/main.cpp
+++ 02.02.2024/main.cpp
@@ -3,15 +3,6 @@ using namespace std;
 
 int main()
 {
-	// 1 найти факториал введенного числа!
-	int a;
-	cin >> a;
-	int fact = 1;
-	while (a > 0)
-	{
-		fact *= a;
-		a--;
-	}
-	cout << fact << endl;
+	cout << "Hello world\n";
 
 }
